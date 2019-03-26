030b68c739079564b0cf807ec87a038d8ec942ca
code-master5 <lauhny.bimalk@gmail.com>
2019-03-27 00:04:22 +0530

    SVM exercise complete

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,26 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
+clf = SVC(kernel="rbf", C=10000)
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+tt = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-tt, 3), "s"
+
+tp = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-tp, 3), "s"
+
+print accuracy_score(pred, labels_test)
+chrisN = 0
+for i in range(len(pred)):
+    if pred[i] == 1:
+        chrisN = chrisN + 1
+print "answer: ", chrisN 
 #########################################################
 
 
